8c42b5cf8016303adeaf49d7902fdb12135d6d94
Zafir <zafirskthelifehacker@hotmail.com>
2024-05-26 18:10:28 +0400

    Error checks

--- app/app/salesforce_integration.py
+++ app/app/salesforce_integration.py
@@ -7,15 +7,17 @@ security_token = 'your_security_token'
 record_id = 'a0OJ7000000L08BMAS'
 
 new_data = {
-    'evttrackr_Status__c': 'Registered'
-    # 'evttrackr_Status__c': 'Not registered'
+    # 'evttrackr_Status__c': 'Registered'
+    'evttrackr_Status__c': 'Not registered'
 }
 
-from simple_salesforce import Salesforce
+sf = Salesforce(username=username, password=password, security_token=security_token)
 
 def my_salesforce_function():
-    sf = Salesforce(username=username, password=password, security_token=security_token)
-
+    if not sf:
+        return False, "Failed to connect to Salesforce"
+    if not record_id:
+        return False, "Record ID is required"
     try:
         sf.evttrackr_EvtTrackrTicket__c.update(record_id, new_data)
         return True, "Record updated successfully", record_id
